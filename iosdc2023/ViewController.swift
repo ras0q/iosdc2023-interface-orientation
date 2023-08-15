8d04a0b280f20d29e8a5ba230e3cc7053ef4a6c9
ras0q <ras0q@users.noreply.github.com>
2023-08-15 16:10:15 +0900

    :sparkles: prior to iOS16: impl rotation

--- iosdc2023/ViewController.swift
+++ iosdc2023/ViewController.swift
@@ -15,6 +15,13 @@ class ViewController: UIViewController {
         return view
     }()
 
+    let rotateButton: UIButton = {
+        let view = UIButton(type: .system)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.setTitle("🔄ROTATE🔄", for: .normal)
+        return view
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,11 +29,40 @@ class ViewController: UIViewController {
 
         // subviews
         view.addSubview(titleLabel)
+        view.addSubview(rotateButton)
 
         // constrains
         NSLayoutConstraint.activate([
             titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 0),
             titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
+            rotateButton.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
+            rotateButton.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
         ])
+
+        // targets
+        rotateButton.addTarget(self, action: #selector(rotate(_:)), for: .touchDown)
+    }
+}
+
+extension ViewController {
+    @available(iOS, obsoleted: 16.0)
+    override var shouldAutorotate: Bool {
+        return false
+    }
+
+    @available(iOS, obsoleted: 16.0)
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.portrait
+    }
+
+    @objc func rotate(_: UIButton) {
+        if #available(iOS 16, *) {
+            print("rotate pushed, but not implemented!")
+        } else {
+            UIDevice.current.setValue(
+                UIInterfaceOrientation.portraitUpsideDown,
+                forKey: "orientation"
+            )
+        }
     }
 }
